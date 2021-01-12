1f9741a84ac92fe9b0d138f355eb587a0d705292
Fred Allen <allenf3@msn.com>
2021-01-12 11:12:09 -0600

    Created abstract JobField class

--- TechJobsOO/JobField.cs
+++ TechJobsOO/JobField.cs
@@ -4,8 +4,37 @@ using System.Text;
 
 namespace TechJobsOO
 {
-    class JobField
+    public abstract class JobField
     {
+        private static int nextId = 0;
+        public int Id { get; set; }
+        public string Value { get; set; }
 
+        public JobField()
+        {
+            Id = nextId;
+            nextId++;
+        }
+
+        public JobField(string value) : this()
+        {
+            Value = value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is JobField field &&
+                   Id == field.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
     }
 }
